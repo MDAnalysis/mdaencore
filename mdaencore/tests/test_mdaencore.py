92a759c87f7b2f7f5d4033e7bb48d2f50871a983
Ian Kenney <ikenney@asu.edu>
2023-07-24 10:58:26 -0700

    Fixed import in tests and updated pip install

--- mdaencore/tests/test_mdaencore.py
+++ mdaencore/tests/test_mdaencore.py
@@ -21,7 +21,7 @@
 # J. Comput. Chem. 32 (2011), 2319--2327, doi:10.1002/jcc.21787
 #
 import MDAnalysis as mda
-import MDAnalysis.analysis.encore as encore
+import mdaencore as encore
 
 import importlib
 import tempfile
